993b18b1b0f0ab39fb68c04e8456ef443c264a8d
Chao Zhang <czhang@axonify.com>
2021-03-16 00:00:32 -0400

    Add 2020A3

---
+++ 2020/A3.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+let count = 0;
+function readline() {
+  return input[count++].split(' ').map(a => +a);
+}
+
+let T = readline();
+for (let i = 1; i <= T; i++) {
+  let [N, K] = readline();
+  let arr = readline();
+  console.log(`Case #${i}: ${solve(arr, N, K)}`);
+}
+
+function solve(arr, N, K) {
+  function check(mid) {   
+    let available = K;
+
+    for (let i = 1; i < N; i++) {
+      let diff = arr[i] - arr[i-1];
+
+      if (diff > mid) {
+        available -= Math.ceil(diff / mid) - 1;
+ 
+        if (available < 0) return false;
+      }
+    }
+ 
+    return true;
+  }
+
+  let l = 1, r = arr[N-1] - arr[0], mid;
+
+  while (l < r) {
+    mid = (l + r) >> 1;
+    
+    if (check(mid)) {
+      r = mid; 
+    } else {
+      l = mid + 1;
+    }
+  } 
+
+  return l;
+}
