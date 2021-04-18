e59cfd4e6d0652e2f25b9dc989937e954feef23c
Chao Zhang <czhang@axonify.com>
2021-04-18 00:21:07 -0400

    Add 2020B2

---
+++ 2020/B/B2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+let count = 0;
+const readnum = () => input[count++].split(' ').map(a => +a);
+const readword = () => input[count++].split(' ');
+
+let T = readnum();
+for (let i = 1; i <= T; i++) {
+  let [N, D] = readnum();
+  let arr = readnum();
+  console.log(`Case #${i}: ${solve(arr, N, D)}`);
+}
+
+function solve(arr, N, D) {
+  for (let i = N - 1; i >= 0; i--) {
+    D -= D % arr[i];
+  }
+
+  return D;
+}
