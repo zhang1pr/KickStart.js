1334a86b2248b84b92cde1b80818354d2ed1410e
Chao Zhang <czhang@axonify.com>
2022-02-16 22:49:33 -0500

    Add 2021A

---
+++ 2021/A/1.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+let count = 0;
+const readnum = () => input[count++].split(' ').map(a => +a);
+const readword = () => input[count++].split(' ');
+
+let T = readline();
+for (let i = 1; i <= T; i++) {
+  let [N, K] = readline();
+  let str = input[count++];
+  console.log(`Case #${i}: ${solve(str, N, K)}`);
+}
+
+function solve(str, N, K) {
+  let res = 0;
+  for (let i=0,j=N-1;i<=j;i++,j--) {
+    if (str[i] != str[j]) {
+      res++;
+    }
+  }
+
+  return Math.abs(K - res);
+}
+
