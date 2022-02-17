9f6169a85d0625d07abfc668e40255bac166c478
Chao Zhang <czhang@axonify.com>
2022-02-16 22:52:53 -0500

    Add 2021G

---
+++ 2021/G/1.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+let count = 0;
+const readnum = () => input[count++].split(' ').map(a => +a);
+const readword = () => input[count++].split(' ');
+
+let T = readnum();
+for (let i = 1; i <= T; i++) {
+  let [N, DF, CF, M] = readnum();
+  let [str] = readword();
+  console.log(`Case #${i}: ${solve(str, DF, CF, M)}`);
+}
+
+function solve(str, DF, CF, M) {
+  let flag = true;
+  for (const ch of str) {
+    if (ch == 'C') {
+      CF--;
+
+      if (CF < 0) {
+        flag = false;
+      }
+    } else {
+      DF--;  
+
+      if (!flag || DF < 0) {
+        return 'NO';
+      }
+
+      CF += M;
+    }
+  }
+
+  return 'YES';
+}
