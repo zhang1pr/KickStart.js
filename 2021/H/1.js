b81a7b0da6542b510523f7f4a2173f2eb07d58ff
Chao Zhang <czhang@axonify.com>
2022-02-16 22:53:10 -0500

    Add 2021H

---
+++ 2021/H/1.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+let count = 0;
+const readnum = () => input[count++].split(' ').map(a => +a);
+const readword = () => input[count++].split(' ');
+
+let T = readnum();
+for (let i = 1; i <= T; i++) {
+  let [str] = readword();
+  let [target] = readword();
+  console.log(`Case #${i}: ${solve(str, target)}`);
+}
+
+function solve(str, target) {
+  let arr = Array(26).fill(Infinity);
+
+  for (let i = 97; i < 97 + 26; i++) {
+    for (let ch of target) {
+      let code = ch.charCodeAt(0);
+
+      if (code < i) {
+        code += 26;
+      }
+
+      arr[i-97] = Math.min(arr[i-97], code - i, i + 26 - code);
+    }
+  }
+
+  let res = 0; 
+  for (let ch of str) {
+    res += arr[ch.charCodeAt(0)-97];
+  }
+
+  return res;
+}
---
+++ 2021/H/2.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+let count = 0;
+const readnum = () => input[count++].split(' ').map(a => +a);
+const readword = () => input[count++].split(' ');
+
+let T = readnum();
+for (let i = 1; i <= T; i++) {
+  let [n] = readnum();
+  let [str] = readword();
+
+  console.log(`Case #${i}: ${solve(str)}`);
+}
+
+function solve(str) {
+  let res = 0;
+  let map = new Map();
+
+  map.set('U', new Set());
+  map.set('R', new Set(['R']));
+  map.set('Y', new Set(['Y']));
+  map.set('B', new Set(['B']));
+  map.set('O', new Set(['R', 'Y']));
+  map.set('P', new Set(['R', 'B']));
+  map.set('G', new Set(['Y', 'B']));
+  map.set('A', new Set(['Y', 'B', 'R']));
+
+  let set = new Set();
+  for (let ch of str) {
+    cur = map.get(ch);
+    for (let color of cur) {
+      if (!set.has(color)) {
+        res++;
+      }
+    }
+
+    set = cur;
+  }
+
+  return res;
+}
